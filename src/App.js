4a2c1cd30811f64fdb59db3a99a5ee0d7998b3c2
AditiBhosale2012 <aditibhosale2012@gmail.com>
2022-03-02 19:39:02 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar2 from './components/Navbar2';
 import Animate from './components/Animate';
 function App() {
   return (
-    <div classname="blank">Lovely
+    <div classname="blank">Lovely aditi here this is new
     <Navbar2/>
     <Navbar title="uppercase" title2="lowecase">
       <Buttons/>
